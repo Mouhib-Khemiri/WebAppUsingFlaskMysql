4ab0617d86523744f66d0a8862d7e5a1eb10b02a
Mouhib-Khemiri <khemiri.mouhib43@gmail.com>
2023-01-17 20:33:35 +0100

    Add a new Route called Signup related to the Sigup button in Index.html it returns a form to SignUp

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def main():
     return render_template("index.html")
 if __name__ == "__main__":
-    app.run(debug="true")
-    
+    app.run()
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
